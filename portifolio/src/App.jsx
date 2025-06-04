09d2fdc86a1d8921396364664b21c95d3fb62d12
ryanmll <fernandespedroryan@gmail.com>
2025-06-04 16:24:27 -0300

    atualizando codigo

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import Header from './components/header/header'
-import NavBar from './components/navbar/navbar'
-import Sobre from './components/sobre/sobre'
+import Header from './components/header/header.jsx'
+import NavBar from './components/navbar/navbar.jsx'
+import Sobre from './components/sobre/sobre.jsx'
 
 function App() {
 
